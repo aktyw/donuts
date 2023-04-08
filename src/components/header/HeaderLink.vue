7f8086ab686eb653539fad49d6234db6dd00ec70
aktyw <arek.tywonek@gmail.com>
2023-04-08 05:26:03 +0200

    feat: navbar overhaul, add icons, simplify components, add mobile view

---
+++ src/components/header/HeaderLink.vue
@@ -0,0 +1,36 @@
+<template>
+  <li>
+    <RouterLink
+      class="btn btn-md lg:btn-sm bg-base-300 border-0 normal-case text-base font-normal text-base-content hover:bg-base-200 rounded"
+      :active-class="isMobile"
+      :to="to">
+      <div class="flex gap-2">
+        <slot name="icon"> </slot>
+        <slot name="name">{{ name }}</slot>
+      </div>
+    </RouterLink>
+  </li>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import type { RouteLocationRaw } from 'vue-router';
+import { getBreakpoints } from '@/composables/useBreakpoints';
+
+const isMobile = computed(() => {
+  if (smAndSmaller) {
+    return 'bg-neutral text-neutral-content hover:text-neutral-content focus:bg-neutral';
+  }
+
+  return 'bg-base-300 text-base-content hover:text-base-300 focus:bg-base-300';
+});
+
+const { smAndSmaller } = getBreakpoints();
+
+type Props = {
+  to: RouteLocationRaw;
+  name?: string;
+};
+
+defineProps<Props>();
+</script>
--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,114 +1,92 @@
 <template>
-  <header class="sticky p-0 navbar bg-accent z-50">
-    <div class="navbar-start">
-      <div class="dropdown">
+  <header class="sticky p-0 px-6 navbar bg-base-300 z-50 min-h-12">
+    <div class="navbar-start flex flex-row-reverse justify-between w-full lg:w-1/2">
+      <div
+        ref="item"
+        class="dropdown dropdown-left [&>.dropdown-content]:right-0 dropdown-bottom gap-2">
         <label
           tabindex="0"
-          class="btn btn-ghost lg:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h8m-8 6h16" />
-          </svg>
+          class="btn btn-sm btn-ghost lg:hidden px-1">
+          <IconList />
         </label>
         <ul
           tabindex="0"
-          class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded w-52">
-          <li class="rounded">
-            <router-link
-              active-class="bg-neutral text-neutral-content "
-              to="/tasks">
-              Tasks
-            </router-link>
-          </li>
-          <li>
-            <router-link
-              active-class="bg-neutral text-neutral-content "
-              to="/notes">
-              Notes
-            </router-link>
-          </li>
+          class="menu menu-compact dropdown-content dropdown-left mt-2 shadow bg-base-200 w-48 gap-2 p-2 rounded-xl"
+          @click="blurDropdown">
+          <HeaderLink to="/tasks">
+            <template #name> Tasks </template>
+          </HeaderLink>
+          <HeaderLink to="/notes">
+            <template #name> Notes </template>
+          </HeaderLink>
+          <HeaderLink to="/">
+            <template #name> About </template>
+          </HeaderLink>
+          <HeaderLink to="/login">
+            <template #name> Login </template>
+          </HeaderLink>
         </ul>
       </div>
 
-      <div class="flex-none">
+      <div class="flex gap-2">
         <BaseButton
-          class="btn btn-square btn-ghost"
+          class="btn btn-sm btn-square btn-ghost"
           @click="settingsStore.toggleMenu()">
           <template #icon>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              class="inline-block w-5 h-5 stroke-current">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
+            <IconMenu />
           </template>
         </BaseButton>
         <BaseButton
-          class="btn btn-square btn-ghost"
+          class="btn btn-sm btn-square btn-ghost"
           @click="router.push({ name: 'project', params: { id: home } })">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24"
-            viewBox="0 96 960 960"
-            width="24">
-            <path
-              d="M240 856h133.847V618.308h212.306V856H720V496L480 315.231 240 496v360Zm-59.999 59.999V466.001L480 240.233l299.999 225.768v449.998H526.154V678.306h-92.308v237.693H180.001ZM480 585.615Z" />
-          </svg>
+          <template #icon>
+            <IconHome />
+          </template>
         </BaseButton>
       </div>
     </div>
+
     <div class="navbar-center hidden lg:flex">
-      <ul class="menu menu-horizontal gap-4">
-        <li>
-          <router-link
-            active-class="bg-neutral text-neutral-content rounded"
-            to="/tasks">
-            Tasks
-          </router-link>
-        </li>
-        <li>
-          <router-link
-            active-class="bg-neutral text-neutral-content rounded"
-            to="/notes">
-            Notes
-          </router-link>
-        </li>
+      <ul class="menu-horizontal gap-4">
+        <HeaderLink to="/tasks">
+          <template #name> Tasks </template>
+        </HeaderLink>
+        <HeaderLink to="/notes">
+          <template #name> Notes </template>
+        </HeaderLink>
       </ul>
     </div>
-    <div class="navbar-end">
-      <router-link
-        to="/"
-        class="btn btn-ghost uppercase">
-        About
-      </router-link>
-      <router-link
-        to="login"
-        class="btn btn-accent">
-        Login
-      </router-link>
+
+    <div class="navbar-end hidden lg:flex">
+      <ul class="flex flex-row gap-4">
+        <HeaderLink to="/">
+          <template #name> About </template>
+        </HeaderLink>
+        <HeaderLink to="/login">
+          <template #name> Login </template>
+        </HeaderLink>
+      </ul>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useSettingsStore } from '@/stores/SettingsStore';
 import { storeToRefs } from 'pinia';
 import BaseButton from '@/components/ui/BaseButton.vue';
 import { useRouter } from 'vue-router';
+import IconMenu from '@/components/icons/IconMenu.vue';
+import IconHome from '@/components/icons/IconHome.vue';
+import IconList from '@/components/icons/IconList.vue';
+import HeaderLink from '@/components/header/HeaderLink.vue';
+import blurElement from '@/helpers/blur';
 
+const item = ref();
+
+function blurDropdown() {
+  blurElement();
+}
 const router = useRouter();
 const settingsStore = useSettingsStore();
 const { getHomeView: home } = storeToRefs(settingsStore);
